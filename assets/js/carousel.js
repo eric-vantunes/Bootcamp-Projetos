cc6619fe7016b675036c2d4fddcc4c307f51253d
Eric Antunes <antunesv.eric@gmail.com>
2022-11-20 19:22:25 -0300

    feat: finalizacoes js

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,45 +1,52 @@
 const palestrantes = [
   {
     image: "person1.png",
-    name: "Juliana",
+    name: "Juliana - Desenvolvedora Fron End",
     title: "Juliana trabalha no Ifood e vai participar do Frontin Sampa",
-    description: "Desenvolvedor Full Stack" 
+    description: "Desenvolvedor Full Stack",
+    video: "https://www.youtube.com/embed/aRJsdYC6zss"
   },
   {
     image: "person2.png",
-    name: "João",
+    name: "João - Arquiteto de Software",
     title: "João trabalha no Ifood e vai participar da Code Con",
-    description: "Desenvolvedor Full Stack" 
+    description: "Desenvolvedor Full Stack",
+    video: "https://www.youtube.com/embed/zwv1hRMO7nU"
   },
   {
     image: "person3.png",
-    name: "Luiz",
+    name: "Luiz - Desenvolvedor Full Stack",
     title: "Luiz trabalha no Ifood e vai participar do Tdc Business",
-    description: "Desenvolvedor Full Stack" 
+    description: "Desenvolvedor Full Stack",
+    video: "https://www.youtube.com/embed/zwv1hRMO7nU"
   },
   {
     image: "person4.png",
-    name: "Tiago",
+    name: "Tiago - Tech Lead",
     title: "Tiago trabalha no Ifood e vai participar do Frontin Sampa",
-    description: "Desenvolvedor Full Stack" 
+    description: "Desenvolvedor Full Stack",
+    video: "https://www.youtube.com/embed/zwv1hRMO7nU"
   },
   {
     image: "person4.png",
-    name: "José",
+    name: "José - Desenvolvedor Back End",
     title: "José trabalha no Ifood e vai participar do Frontin Sampa",
-    description: "Desenvolvedor Full Stack" 
+    description: "Desenvolvedor Full Stack",
+    video: "https://www.youtube.com/embed/zwv1hRMO7nU"
   },
   {
     image: "person1.png",
-    name: "Lorena",
+    name: "Lorena Devops",
     title: "Lorena trabalha no Ifood e vai participar do Frontin Sampa",
-    description: "Desenvolvedor Full Stack" 
+    description: "Desenvolvedor Full Stack",
+    video: "https://www.youtube.com/embed/zwv1hRMO7nU"
   },
   {
     image: "person1.png",
-    name: "Laura",
+    name: "Laura Desenvolvedora Front End",
     title: "Laura trabalha no Ifood e vai participar do Frontin Sampa",
-    description: "Desenvolvedor Full Stack" 
+    description: "Desenvolvedor Full Stack",
+    video: "https://www.youtube.com/embed/zwv1hRMO7nU"
   },
 ];
 
@@ -50,6 +57,7 @@ function openModal(elemento, idx) {
   const palestranteItem = palestrantes[idx];
   document.querySelector(`${elemento} #palestranteModalLabel`).innerText = palestranteItem.title;
   document.querySelector(`${elemento} .modal-body`).innerHTML = `
+    <iframe width="100%" height="300" src="${palestranteItem.video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <p>${palestranteItem.description}</p>
   `;
 }
@@ -80,6 +88,7 @@ class Carousel{
     this.elementosCarousel.innerHTML = itensPalestrante;
   }
 
+  // Slick
     initCarousel(){
       $(".palestrantes").slick({
         infinite: true,
